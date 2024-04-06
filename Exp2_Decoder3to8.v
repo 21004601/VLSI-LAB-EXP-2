39edf9ea128a5912cc57a397443a32a923990d3b
21004601 <146088220+21004601@users.noreply.github.com>
2024-04-06 18:30:14 +0530

    Update Exp2_Decoder3to8.v

--- Exp2_Decoder3to8.v
+++ Exp2_Decoder3to8.v
@@ -1,7 +1,8 @@
-module decoder_8(a,b,c,y);
-input a,b,c;
-output[7:0]y;
-
-
-
+module decoder(a0,a1,d0,d1,d2,d3);
+input a0,a1;
+output d0,d1,d2,d3;
+assign d0=~a1&~a0;
+assign d1=~a1&a0;
+assign d2=a1&~a0;
+assign d3=a0&a1;
 endmodule
