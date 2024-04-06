667b3f7007c26db3d64fbd9f855899309bf59d31
21004601 <146088220+21004601@users.noreply.github.com>
2024-04-06 18:37:55 +0530

    Update Exp2_Magnitudecomparator.v

--- Exp2_Magnitudecomparator.v
+++ Exp2_Magnitudecomparator.v
@@ -1,10 +1,25 @@
-Module comparator(a,b,eq,lt,gt);
-input [3:0] a,b;
-output reg eq,lt,gt;
-always @(a,b)
+module comparator(a,b,x,y,z);
+input [3:0]a,b;
+output reg x,y,z;
+always@(a,b)
 begin
- 
- 
- 
-end 
+if(a>b)
+begin
+x=1'b1;
+y=1'b0;
+z=1'b0;
+end
+else if(a<b)
+begin
+x=1'b0;
+y=1'b1;
+z=1'b0;
+end
+else
+begin
+x=1'b0;
+y=1'b0;
+z=1'b1;
+end
+end
 endmodule
