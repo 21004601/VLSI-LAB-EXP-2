b3fc2383a6620089ff6967c0e73b662833ff5179
21004601 <146088220+21004601@users.noreply.github.com>
2024-04-06 18:32:37 +0530

    Update Exp2_Demultiplexer1to8.v

--- Exp2_Demultiplexer1to8.v
+++ Exp2_Demultiplexer1to8.v
@@ -1,9 +1,12 @@
-module demux_8(s,d,y);
-input [2:0]s;
-input d;
-output [7:0]y;
-
-
-
-
+module demux_1_to_8(d,s0,s1,s2,y0,y1,y2,y3,y4,y5,y6,y7);
+input d,s0,s1,s2;
+output y0,y1,y2,y3,y4,y5,y6,y7;
+and g4(y0,d,~s0,~s1,~s2);
+and g5(y1,d,~s0,~s1,s2);
+and g6(y2,d,~s0,s1,~s2);
+and g7(y3,d,~s0,s1,s2);
+and g8(y4,d,s0,~s1,~s2);
+and g9(y5,d,s0,~s1,s2);
+and g10(y6,d,s0,s1,~s2);
+and g11(y7,d,s0,s1,s2);
 endmodule
