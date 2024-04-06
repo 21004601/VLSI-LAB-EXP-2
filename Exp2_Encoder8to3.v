d5a4c9333c7477e2aeaa193938edc9b1aa74dab2
21004601 <146088220+21004601@users.noreply.github.com>
2024-04-06 18:34:22 +0530

    Update Exp2_Encoder8to3.v

--- Exp2_Encoder8to3.v
+++ Exp2_Encoder8to3.v
@@ -1,7 +1,7 @@
-module encoder_8(d,a,b,c);
-input [7:0]d;
-output a,b,c;
-
-
-
+module encoder(a2,a1,a0,y0,y1,y2,y3,y4,y5,y6,y7);
+input y0,y1,y2,y3,y4,y5,y6,y7;
+output a0,a1,a2;
+or g1(a0,y0,y3,y5,y7);
+or g2(a1,y1,y2,y3,y[6],y[7]);
+or g3(a2,y[4],y[5],y[6],y[7]);
 endmodule
